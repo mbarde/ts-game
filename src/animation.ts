c054f765b2f0c516d2f45de4443fa30b75f6f72b
Matthias Barde <matthiasbarde@gmx.de>
2022-08-26 22:42:23 +0200

    first render cycle

---
+++ src/animation.ts
@@ -0,0 +1,61 @@
+export class AnimatedProperty {
+
+  private delta: number;
+  private max: number;
+  private min: number;
+  private value: number;
+  private oscillation: boolean;
+
+  constructor(min: number, max: number, delta: number) {
+    this.max = max;
+    this.min = min;
+    this.delta = delta;
+    this.value = Math.floor((this.max - this.min)/2) + this.min;
+    this.oscillation = true;
+  }
+
+  update() {
+    this.value += this.delta;
+    if (this.oscillation === true) {
+      if ( (this.value <= this.min && this.delta < 0) ||
+           (this.value >= this.max && this.delta > 0) ) {
+        this.delta = -this.delta;
+      }
+    }
+    if (this.value < this.min) this.value = this.min;
+    if (this.value > this.max) this.value = this.max;
+  }
+
+  setToMax() {
+    this.value = this.max;
+  }
+
+  setToMin() {
+    this.value = this.min;
+  }
+
+  getValue(): number {
+    return this.value;
+  }
+
+  setDelta(delta: number) {
+    this.delta = delta;
+  }
+
+  setMax(max: number) {
+    this.max = max;
+  }
+
+  setMin(min: number) {
+    this.min = min;
+  }
+
+  setValue(value: number) {
+    this.value = value;
+  }
+
+  setOscillation(oscillation: boolean) {
+    this.oscillation = oscillation;
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('hello, friend');
+import { AnimatedProperty } from './animation';
+
+const canvas = document.getElementById('canvas');
+
+const ball = document.createElement('div');
+ball.classList.add('ball');
+ball.style.position = 'absolute';
+
+canvas.append(ball);
+
+let x: number = 0;
+let y: number = 0;
+let rot: number = 0;
+
+let lastTick: number = Date.now();
+
+let size: AnimatedProperty = new AnimatedProperty(1.0, 1.5, 0.001);
+
+const update = (ball: HTMLElement) => {
+    let now, ticks: number;
+    now = Date.now();
+    ticks = now - lastTick;
+    lastTick = now;
+    x += (ticks / 50);
+    y += (ticks / 40);
+    rot += (ticks / 50);
+    rot = rot > 360 ? rot - 360 : rot;
+    size.update();
+    ball.style.left = `${x}px`;
+    ball.style.top = `${y}px`;
+    ball.style.transform = `rotate(${rot}deg) scale(${size.getValue()})`;
+}
+
+setInterval(() => update(ball), 1);
